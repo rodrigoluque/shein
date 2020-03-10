3bae790f196a7f06c00931d33e735186a86a8bda
rodrigoluque <rodrigoluque07@gmail.com>
2020-03-09 23:27:01 -0300

    Se sube catalogo.sql con nuevos datos para prdImagen

--- BASE_DE_DATOS_ECOMMERCE/catalogo.sql
+++ BASE_DE_DATOS_ECOMMERCE/catalogo.sql
@@ -80,19 +80,21 @@ CREATE TABLE IF NOT EXISTS `productos` (
   KEY `Rubro` (`idCategoria`),
   CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`idMarca`) REFERENCES `marcas` (`idMarca`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `productos_ibfk_2` FOREIGN KEY (`idCategoria`) REFERENCES `categorias` (`idCategoria`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
--- Volcando datos para la tabla catalogo.productos: ~7 rows (aproximadamente)
+-- Volcando datos para la tabla catalogo.productos: ~9 rows (aproximadamente)
 DELETE FROM `productos`;
 /*!40000 ALTER TABLE `productos` DISABLE KEYS */;
 INSERT INTO `productos` (`idProducto`, `prdNombre`, `prdPrecio`, `idMarca`, `idCategoria`, `prdPresentacion`, `prdStock`, `prdImagen`) VALUES
-	(2, 'Hooded Plain Coat', 2000, 14, 1, 'cotton,women, style, clothing_length', 50, 'DJeFGeZ41gOSotSExQowg8T2S9x9j7kqOFzndKbk.jpeg'),
-	(3, 'Abrigo Impermeable', 1500, 11, 3, 'Hink de Lluvia,Impermeable con Capucha', 15, 'HY69XHnA5aOEXwUXMiEczUCrt7Hk4NTudG8mCW6T.jpeg'),
-	(4, 'Wantdo Abrigo', 3500, 5, 4, 'Doble Botonadura con Cinturón,', 27, '5HEsNRoYijmu1QrBDecEvBptug6siwDViyPaif0Q.jpeg'),
-	(5, 'Long Sleeve', 4500, 12, 4, 'Collar Buttons Coats, de paño', 45, 'l3EEb4TUqhTdcIH1G4iE8PseItehNqvtFATiZqGv.jpeg'),
+	(2, 'Hooded Plain Coat', 2000, 2, 1, 'cotton,women, style, clothing_length', 50, 'img2.jpeg'),
+	(3, 'Abrigo Impermeable', 1500, 11, 6, 'Hink de Lluvia,Impermeable con Capucha', 15, 'HY69XHnA5aOEXwUXMiEczUCrt7Hk4NTudG8mCW6T.jpeg'),
+	(4, 'Wantdo Abrigo', 3500, 5, 3, 'Doble Botonadura con Cinturón,', 27, '5HEsNRoYijmu1QrBDecEvBptug6siwDViyPaif0Q.jpeg'),
+	(5, 'Long Sleeve', 4500, 6, 3, 'Collar Buttons Coats, de paño', 45, 'l3EEb4TUqhTdcIH1G4iE8PseItehNqvtFATiZqGv.jpeg'),
 	(6, 'Parka', 2800, 3, 1, 'Impermeable', 35, 'mqba3xoshT9yowvq0J7TkXmtkTSkzs2sjG4KZmuu.jpeg'),
 	(7, 'Lunch Cooler Bag', 1200, 9, 2, 'Customizeable,negro', 80, 'JqZGgv2YiXL5vb3CuDea9obS61CXTB6iWJJKifGB.jpeg'),
-	(8, 'Roya Puffer', 4500, 11, 6, 'Pongee down filled puffer', 5, 'Is6fJWXsU1Z8Zmo0P5zgCEVuGnsNFyvnY3SsWsh9.jpeg');
+	(8, 'Roya Puffer', 4500, 12, 1, 'Pongee down filled puffer,blanco', 5, 'DJeFGeZ41gOSotSExQowg8T2S9x9j7kqOFzndKbk.jpeg'),
+	(9, 'Kie Sweet', 8000, 12, 1, 'Sweet cotton', 10, 'img3.jpg'),
+	(10, 'Kirk blue', 3200, 10, 1, 'Tela jean', 36, 'mmZEeLxnbp3nRTw26l3ja1Klk4LPg3f4mHHJ96nE.jpeg');
 /*!40000 ALTER TABLE `productos` ENABLE KEYS */;
 
 -- Volcando estructura para tabla catalogo.usuarios
