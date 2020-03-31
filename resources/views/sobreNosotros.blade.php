9c1d75af4dab2fe5dd85ceb7989301b87f7f35ce
tamahermo <tamahermosilla1@gmail.com>
2020-03-31 17:10:57 -0300

    retoque en vista sobreNosotros

--- resources/views/sobreNosotros.blade.php
+++ resources/views/sobreNosotros.blade.php
@@ -11,32 +11,46 @@
 
 
 
-<div class="container">
-  <h3>Camperas</h3><br>
+<div class="container mt-5">
+<div class="navspacer p-4 mt-3">
+
+  <h3> SHEIN </h3>
   <p>
-    Somos una marca Argentina de indumentaria fundada en el año 2019.<br>
+    Somos una Marca de Inumentaria  Argentina fundada en el Año 2019<br>
 
-    Nacimos en un pequeño local en una galería de Buenos Aires.<br>
+    Nacimos en un pequeño local en una galería de Buenos Aires<br>
 
-    Desde nuestros inicios tuvimos un crecimiento constante y sostenido llegando a ser hoy una de las marcas más importantes en el mercado nacional.<br>
+    Desde nuestros inicios tuvimos un crecimiento constante y sostenido,<br>
+     llegando a ser hoy, una de las marcas más importantes en el MERCADO NACIONAL.<br>
 
-    Nuestro compromiso es ofrecer una gran variedad de prendas con una excelente relación diseño-precio-calidad.<br>
+    Nuestro compromiso es ofrecer una gran variedad de prendas<br>
+     con una excelente relación diseño-precio-calidad.<br>
 
-    Trabajamos para tener una constante renovación de productos, lo que nos ayuda a brindarle  una gran diversidad de prendas de moda para que pueda usar en diferentes momentos del día..<br>
+    Trabajamos para tener una constante renovación de productos, lo que nos ayuda a brindarle<br>
+     una gran diversidad de prendas de moda para que pueda usar en diferentes momentos del día..<br>
 
   </p>
+  
 
   <h3>Nuestros Locales</h3>
-  <p>   ABASTO SHOPPING<br>
-  4861-4968 / 4865-7113<br>
-  ADROGUE BOULEVARD<br>
-  Avenida Hipólito Yrigoyen 169<br>
-  4239-1169/4214-4670
-</p>
+<details>
+  <summary> ABASTO SHOPPING</summary>
+  <ul>
+  <li>4861-4968 </li>
+  <li> 4865-7113<li>
+  </ul>
+  </details>
+  <details>
+  <summary> ADROGUE BOULEVARD </summary>
+  <ul>
+  <li>4239-1169</li>
+  <li>4214-4670</li>
+  </ul>
+</details>
 
 
 </div>
-
+</div>
 <br>
 
 @endsection
